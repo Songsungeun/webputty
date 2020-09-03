0023d37c3edce3bbf291b9867284d5231dbca0f3
Songsungeun <korea.sesong@gmail.com>
2020-09-03 15:28:31 +0900

    📑Terminal onKey Event 추가

--- src/front/components/Term.ts
+++ src/front/components/Term.ts
@@ -3,41 +3,87 @@ import { FitAddon } from 'xterm-addon-fit';
 import '../css/term.css';
 import { Socket } from 'socket.io-client';
 
+// SECTION - Terminal
 export default class Term {
     section = document.createElement('section');
+    private _term: Terminal | null = null;
+    private _sock: SocketIOClient.Socket | null = null;
+    private pw: string = '';
+    account: string = '';
+    backSpackLimit: number = 0;
 
     constructor(target: HTMLElement) {
         this.section.className = 'term_area';
+
         target.appendChild(this.section);
         this.render();
     }
 
     render() {
-        const term = this.openTerminal();
-        this.initTerminal(term);
-        const socket = this.openSocket();
+        this.openTerminal();
+        this.initTerminal();
     }
 
+    // ANCHOR - get, set (Terminal, Socket)
+    setTerm(term: Terminal) {
+        this._term = term;
+    }
+
+    getTerm(): Terminal {
+        return this._term as Terminal;
+    }
+
+    setSocket(sock: SocketIOClient.Socket) {
+        this._sock = sock;
+    }
+
+    getSocket() {
+        return this._sock;
+    }
+
+    // NOTE - Func for Terminal
     openTerminal() {
         const terminal = new Terminal();
+        this.setTerm(terminal);
         const fitAddon = new FitAddon();
         terminal.loadAddon(fitAddon);
         terminal.open(this.section);
         fitAddon.fit();
-        return terminal;
     }
 
-    initTerminal(term: Terminal) {
-        term.writeln('Welcome to Web Putty!');
+    initTerminal() {
+        let term = this.getTerm();
+        term.writeln('Welcome to Web Putty! - by.Songsungeun');
         term.writeln('');
     }
 
-    addEventtoTerminal(term: Terminal) {
+    addEventToTerminal() {
+        let term = this.getTerm();
 
+        let onKey = term.onKey(({ key }) => {
+            if (key === '\u007F') {
+                if (term.buffer.active.cursorX > this.backSpackLimit) {
+                    term.write('\b \b');
+                }
+            }
+            term.write(key);
+        })
     }
 
+    readyToConnect(ip: String) {
+        let term = this.getTerm();
+        let AccountTermString: string = 'Login as: ';
+        this.backSpackLimit = AccountTermString.length;
+
+        term.writeln(`connect to ${ip}`);
+        term.write(AccountTermString);
+        this.addEventToTerminal();
+    }
+
+    // NOTE - Func for SOCKET
     openSocket() {
         const socket: SocketIOClient.Socket = io();
-        return socket;
+        this.setSocket(socket);
     }
 }
+// !SECTION
