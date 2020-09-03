a8c701366bcdc8405b6151aaca466d5f188d2b32
Songsungeun <korea.sesong@gmail.com>
2020-09-03 13:20:10 +0900

    📑Header 추가

--- src/front/components/Header.ts
+++ src/front/components/Header.ts
@@ -1,4 +1,5 @@
 export default class Header {
+    // SECTION - Header
     section = document.createElement('header');
 
     constructor(target: HTMLElement) {
@@ -8,7 +9,27 @@ export default class Header {
     }
 
     render() {
-        const leftArea = document.createElement('div');
-        this.section.appendChild(leftArea);
+
+        const headerItem = document.createElement('div');
+        headerItem.classList.add('header_itemBox', 'releative', 'items-center', 'flex-nowrap', 'flex-row', 'flex');
+
+        const TitleBox = document.createElement('h4');
+        TitleBox.classList.add('header_item');
+        TitleBox.innerText = 'Web Putty';
+        headerItem.appendChild(TitleBox);
+
+        const ipInput = document.createElement('input');
+        ipInput.placeholder = 'IP Address';
+        ipInput.classList.add('header_item', 'bg-gray1', 'ip_input');
+        headerItem.appendChild(ipInput);
+
+        const connectBtn = document.createElement('button');
+        connectBtn.classList.add('header_item', 'connect_btn');
+        connectBtn.innerText = 'Connect!';
+        headerItem.appendChild(connectBtn);
+
+        this.section.appendChild(headerItem);
     }
+
+    // !SECTION
 }
