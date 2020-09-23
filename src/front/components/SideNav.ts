2c3dd728b5d46220f0afdd354dd2c0b6766f1bea
Songsungeun <korea.sesong@gmail.com>
2020-09-23 17:39:01 +0900

    navbar class 추가(리스트 아이템 구현 전)

--- src/front/components/SideNav.ts
+++ src/front/components/SideNav.ts
@@ -1,15 +1,31 @@
 import { DataManagement } from "../data/DataManagement";
 
 export default class SideNav {
-    section = document.createElement('section');
+    store: DataManagement;
 
     constructor({ target, dataState }: { target: HTMLElement, dataState: DataManagement }) {
-        this.section.classList.add('side_nav');
-        target.appendChild(this.section);
+        this.store = dataState;
+        const section = document.createElement('section');
+        section.classList.add('side_nav');
+        target.appendChild(section);
         this.render();
     }
 
     render() {
+        let history = this.store.findAllArray();
 
+        // TODO - NAV 바에 SideItems 추가
+    }
+
+
+}
+
+class SideItems {
+
+    constructor($target: HTMLElement, nick: string, ip: string) {
+        let section = document.createElement('div');
+        const nickName = nick;
+        const ipAddr = ip;
+        $target.appendChild(section);
     }
 }
