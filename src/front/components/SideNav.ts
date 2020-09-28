30fca5181b5497c27071a678254319051e3dc530
Songsungeun <korea.sesong@gmail.com>
2020-09-28 16:13:56 +0900

    이전에 연결한 리스트 추가

--- src/front/components/SideNav.ts
+++ src/front/components/SideNav.ts
@@ -2,30 +2,60 @@ import { DataManagement } from "../data/DataManagement";
 
 export default class SideNav {
     store: DataManagement;
+    section: null | HTMLElement;
 
     constructor({ target, dataState }: { target: HTMLElement, dataState: DataManagement }) {
         this.store = dataState;
-        const section = document.createElement('section');
-        section.classList.add('side_nav');
-        target.appendChild(section);
+        this.section = document.createElement('section');
+        this.section.classList.add('side_nav');
+        target.appendChild(this.section);
         this.render();
     }
 
     render() {
-        let history = this.store.findAllArray();
+        let history = this.store.findAll();
 
         // TODO - NAV 바에 SideItems 추가
+        const historyWrap = document.createElement('ul');
+        historyWrap.classList.add('iplist');
+
+        for (let key in history) {
+            new SideItem({
+                $target: historyWrap,
+                nick: key,
+                ip: history[key]
+            })
+        }
+
+        // TODO - add Event
+        this.section?.appendChild(historyWrap);
     }
 
 
 }
 
-class SideItems {
+class SideItem {
+    public nickName: string
+    public ip: string
+    public section: HTMLLIElement
 
-    constructor($target: HTMLElement, nick: string, ip: string) {
-        let section = document.createElement('div');
-        const nickName = nick;
-        const ipAddr = ip;
-        $target.appendChild(section);
+    constructor({ $target, nick, ip }: { $target: HTMLUListElement, nick: string, ip: string }) {
+        this.section = document.createElement('li');
+        this.section.className = 'site_item';
+
+        this.nickName = nick;
+        this.ip = ip;
+
+        this.render();
+        $target.appendChild(this.section);
+    }
+
+    render() {
+        const link = document.createElement('a');
+        link.classList.add('ipitem');
+        link.href = '#'
+        link.dataset.addr = this.ip;
+        link.innerText = this.nickName.includes('noname') ? this.ip : this.nickName;
+        this.section.appendChild(link);
     }
 }
